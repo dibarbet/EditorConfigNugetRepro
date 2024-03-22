9ce8f610bed39d8b672e918d4478c3d978f8a55f
David Barbet <dabarbet@microsoft.com>
2024-03-21 21:49:38 -0700

    more

--- repro.ps1
+++ repro.ps1
@@ -1,6 +1,7 @@
+$env:MSBUILDLOGALLENVIRONMENTVARIABLES=1
 Write-Host ""
 Write-Host "---- Running *WITHOUT* trailing slash ----" -ForegroundColor Green
-$env:NUGET_PACKAGES=(Join-Path $PSScriptRoot "packages")
+$env:NUGET_PACKAGES="$PSScriptRoot\\packages"
 Write-Host "Using NUGET_PACKAGES directory: $env:NUGET_PACKAGES" -ForegroundColor Green
 
 git clean -xdf
@@ -8,13 +9,13 @@ Write-Host "Creating nuget package with editorconfig..." -ForegroundColor Green
 dotnet build ./NugetPackageWithEditorConfig/ --no-incremental
 dotnet pack ./NugetPackageWithEditorConfig/ --configuration Debug
 Write-Host "Rebuilding consuming package..." -ForegroundColor Green
-dotnet build ./ConsumeNuget/ --no-incremental
+dotnet build ./ConsumeNuget/ --no-incremental -bl:C:\Users\dabarbet\source\repos\msbuild.binlog
 
 Write-Host ""
 Write-Host ""
 
 Write-Host "---- Running *WITH* trailing slash ----" -ForegroundColor Green
-$env:NUGET_PACKAGES=(Join-Path $PSScriptRoot "packages/")
+$env:NUGET_PACKAGES="$PSScriptRoot\\packages\"
 Write-Host "Using NUGET_PACKAGES directory: $env:NUGET_PACKAGES" -ForegroundColor Green
 
 git clean -xdf
